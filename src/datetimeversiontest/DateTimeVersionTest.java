aad1e7ed9268d69699f06e78ad0989378af9d1a7
Caolan <caolan@ocluk.com>
2017-07-03 14:04:29 +0100

    First Commit

---
+++ src/datetimeversiontest/DateTimeVersionTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datetimeversiontest;
+
+import java.util.*;
+
+/**
+ *
+ * @author Caolán
+ */
+public class DateTimeVersionTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Date today = Calendar.getInstance().getTime();
+        
+        System.out.println("Version 1.00");
+        System.out.println(today);
+        
+    }
+    
+}
